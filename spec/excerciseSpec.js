5a8b29c05eadaf76314aed2b2db9ce56b4a07917
amhayslip <amhayslip@gmail.com>
2015-11-10 12:13:32 +0200

    add setup and first tests

---
+++ spec/excerciseSpec.js
@@ -0,0 +1,40 @@
+////////// Excercise 1 //////////
+var dog = {
+  name: "Rover",
+  legs: 4
+};
+
+// Write code here to make the test below pass
+
+describe("The Dog", function () {
+  it("Has a name and a color", function () {
+    expect(dog['name']).toEqual("Rover");
+    expect(dog['color']).toEqual("brown");
+  });
+});
+
+////////// Excercise 2 //////////
+
+// Write code here to make the test below pass
+
+describe("The Person", function () {
+  it("Has a name and an age", function () {
+    expect(person['name']).toBeDefined();
+    expect(person['age']).toBeDefined();
+  });
+});
+
+////////// Excercise 3 //////////
+var book = {
+	title: "The Hunger Games",
+	author: "J.K. Rowling "
+}
+
+// Write code here to make the test below pass... do not change the code above
+
+describe("The Hunger Games", function () {
+	it("Has correct author", function () {
+		expect(book.title).toEqual("The Hunger Games");
+		expect(book.author).toEqual("Suzanne Collins");
+	});
+})
