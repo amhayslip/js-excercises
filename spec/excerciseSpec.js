5b7bc3b316fadd1ed0aa4ed6b5292f3893749d2b
amhayslip <amhayslip@gmail.com>
2015-11-10 14:31:12 +0200

    Add test for this

--- spec/excerciseSpec.js
+++ spec/excerciseSpec.js
@@ -1,16 +1,36 @@
 ////////// Excercise 1 //////////
-var posts = {
-  comments: [
-    {user: "Aaron", message: "Hey there!"},
-    {user: "Avi", message: "Man, this course is awesome!"},
-    {user: "Narkis", message: "It's pretty good, but whats your vision?"}
-  ]
+var person = {
+	eat: function () {
+		this.hungry = !this.hungry;
+	}
+};
+
+person.eat();
+
+// Add a default attribute to the person object to make the test below pass
+
+describe("Person", function () {
+  it("is not hungry after she eats, but is before", function () {
+    expect(person.hungry).toEqual(false);
+  });
+});
+
+////////// Excercise 2 //////////
+var bank = {
+	accounts: [
+		{ name: "Aaron", balance: 200 },
+		{ name: "Avi", balance: 5 },
+		{ name: "Narkis", balance: 500 }
+	],
+	deposit: function (index, amount) {
+		this.accounts[index].balance += amount;
+	}
 }
 
-// Write code here to make the test below pass. Hint: you will need to "push" a new object into the comments array.
+// Write code here to make the test below pass. Do no change any code above.
 
-describe("Posts", function () {
-  it("Has a comment from Alex", function () {
-    expect(posts.comments[3].user).toEqual("Alex");
+describe("Bank", function () {
+  it("account for Aaron has an increased balance", function () {
+    expect(bank.accounts[0].balance).toEqual(300);
   });
 });
