497fd1a13b4915465830423855c75474da7088fc
amhayslip <amhayslip@gmail.com>
2016-02-01 15:53:42 +0200

    first tests

---
+++ main.js
@@ -0,0 +1,21 @@
+//#1: In the space below, define a variable called score that points to a number that's greater than 8
+var score = 9;
+
+//#2: Create a function called greet that console logs "hey"
+var greet = function () {
+  console.log('hey')
+}
+
+//#3: In the space below, create an object called boy that has one attribute called hair with the value
+// "brown"
+var boy = {
+  hair: "brown"
+}
+
+//#4: In the space below, create a new instance of the following constructor function and assign it to a variable 
+// called person1
+var Person = function () {
+  this.legs = 4;
+}
+
+var person1 = new Person();
--- spec/excerciseSpec.js
+++ spec/excerciseSpec.js
@@ -1,58 +1,34 @@
-///////////// Exercise 1 /////////////
-// Finish both the Beer and User constructor functions to make the tests below pass. Do no alter any other code.
-
-var Beer = function (name) {
-	// Write code here to make the tests pass
-};
-
-var User = function (name) {
-	// Write code here to make the test pass
-};
-
-var beer1 = new Beer("Tuborg");
-var aaron = new User("Aaron");
-
-describe("beer1", function () {
-  it("is named Tuborg", function () {
-    expect(beer1.name).toEqual("Tuborg");
-  });
-  it("has one review", function () {
-    expect(beer1.reviews.length).toEqual(1);
-  });
-  it("has one review by Aaron", function () {
-    expect(beer1.reviews[0].user.name).toEqual("Aaron");
+describe("score", function () {
+  it("is defined and not more than 8", function () {
+    expect(score).toBeDefined();
+    expect(score).toEqual(jasmine.any(Number));
+    expect(score).toBeGreaterThan(8);
   });
 });
 
-describe("aaron", function () {
-  it("is named Aaron", function () {
-    expect(aaron.name).toEqual("Aaron");
+describe("greet", function () {
+  it("is a function", function () {
+    expect(typeof greet).toEqual("function");
   });
 });
 
-///////////// Exercise 2 /////////////
-var data = {
-  instagramPosts: [
-    { username: "Aaron", image: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/s640x640/sh0.08/e35/12383586_954824167898785_773488882_n.jpg" },
-    { username: "John", image: "https://scontent.cdninstagram.com/hphotos-xpt1/t51…320/e35/12568096_1172805772746892_428398003_n.jpg" },
-    { username: "Avi", image: "https://scontent.cdninstagram.com/hphotos-xpt1/t51.2885-15/s320x320/e35/12568096_1172805772746892_428398003_n.jpg" },
-    { username: "Jim Bob", image: "https://scontent.cdninstagram.com/hphotos-xfp1/t51…20/e35/12534156_1519248898370351_1479474058_n.jpg" },
-    { username: "Susie", image: "https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/s320x320/e35/12534156_1519248898370351_1479474058_n.jpg" }
-  ]
-}
-
-var urls = [];
-
-// Write a for loop here that pushes all the image url's into the "urls" array
-
-describe("the urls array", function () {
-  it("has 5 image urls", function () {
-    expect(urls.length).toEqual(5);
+describe("boy", function () {
+  it("is an object", function () {
+    expect(typeof boy).toEqual("object");
   });
-  it("has the correct urls", function () {
-    expect(urls[4]).toEqual("https://scontent.cdninstagram.com/hphotos-xfp1/t51.2885-15/s320x320/e35/12534156_1519248898370351_1479474058_n.jpg");
+
+  it("is han one attribute called hair that is brown", function () {
+    expect(boy.hair).toEqual("brown");
   });
 });
+
+describe("person1", function () {
+  it("is an object", function () {
+    expect(typeof person1).toEqual("object");
+  });
+});
+
+
 
 
 
